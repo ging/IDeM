926561a6099041335df9ec0fcadd50d52bc84215
agordillo <agordillos@gmail.com>
2016-04-13 14:13:32 +0200

    Allow to show documents without being logged

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:show]
   before_filter :fill_create_params, :only => [:create]
 
   def create
