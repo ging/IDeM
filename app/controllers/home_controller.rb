f7482445b04802532ab18d0fe50998aa5ac96479
aalonsog <aalonsog@dit.upm.es>
2016-05-03 15:22:09 +0200

    Publications in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @presentations = RecommenderSystem.suggestions({:n => 20, :user_profile => current_user_profile, :settings => {:database => "IDeM", :preselection_filter_by_resource_types => ["Presentation"]}})
     @webinars = RecommenderSystem.suggestions({:n => 20, :user_profile => current_user_profile, :settings => {:database => "IDeM", :preselection_filter_by_resource_types => ["Webinar"]}})
     @recordings = RecommenderSystem.suggestions({:n => 20, :user_profile => current_user_profile, :settings => {:database => "IDeM", :preselection_filter_by_resource_types => ["Recording"]}})
-    @recommended_publications = RecommenderSystem.suggestions({:n => 20, :user_profile => current_user_profile, :settings => {:database => "Loop", :preselection_filter_by_resource_types => ["Publication"]}})
+    @publications = RecommenderSystem.suggestions({:n => 20, :user_profile => current_user_profile, :settings => {:database => "Loop", :preselection_filter_by_resource_types => ["Publication"]}})
   end
 
 end
